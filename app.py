f8ef2baf6808b08fc726a48088ca201eb67609c9
Jack Wong <24000526g@connect.polyu.hk>
2024-11-05 02:32:06 +0000

    add progress text

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import os
 import torch
 import streamlit as st
 import io
 from pydub import AudioSegment
 from tqdm import tqdm
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoProcessor, pipeline
+import os
 
-st.title("Cantonese ASR")
+st.title("Cantonese ASR Transcription")
 
 # Initialize session state for tracking processing status
 if 'processed' not in st.session_state:
@@ -15,15 +15,20 @@ if 'processed' not in st.session_state:
 # File uploader with drag-and-drop functionality
 uploaded_file = st.file_uploader("Upload your video file", type=["mp4"], key="file_uploader")
 
+# Reset the processed flag if a new file is uploaded
+if uploaded_file is not None:
+    st.session_state.processed = False
+
 if uploaded_file is not None and not st.session_state.processed:
     lang = "zh"
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     model_id = "alvanlii/whisper-small-cantonese"
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model = AutoModelForSeq2SeqLM.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
     )
     model.to(device)
+
     processor = AutoProcessor.from_pretrained(model_id)
     pipe = pipeline(
         "automatic-speech-recognition",
@@ -44,21 +49,30 @@ if uploaded_file is not None and not st.session_state.processed:
 
     transcription = ""
     progress_bar = st.progress(0)
+    progress_text = st.empty()
     total_chunks = len(chunks)
 
     for i, chunk in enumerate(tqdm(chunks, desc="Processing audio")):
         result = pipe(chunk.export(format="wav").read())
         transcription += result['text'] + " "
-        progress_bar.progress((i + 1) / total_chunks)
+        progress = (i + 1) / total_chunks
+        progress_bar.progress(progress)
+        progress_text.text(f"Progress: {int(progress * 100)}%")
 
     st.title("Result")
     with st.expander("Transcription"):
         st.info(transcription)
 
+    # Get the filename without the extension
+    filename_without_extension = os.path.splitext(uploaded_file.name)[0]
+
+    # Use the filename without the extension for the output file name
+    output_file_name = f"{filename_without_extension}_transcription.txt"
+
     # Create a download button for the output file
     st.download_button(
         label="Download Transcription",
-        file_name=f"{os.path.splitext(uploaded_file.name)[0]}_transcription.txt",
+        file_name=output_file_name,
         mime="text/plain",
         data=transcription.encode('utf-8')
     )

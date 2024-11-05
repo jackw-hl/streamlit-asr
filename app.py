eed37007152358805efba5c38dc705b52e6b9989
Jack Wong <24000526g@connect.polyu.hk>
2024-11-05 07:25:07 +0000

    debug

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if uploaded_file is not None and not st.session_state.processed:
     # Process audio
     audio_bytes = uploaded_file.read()
     audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="mp4")
-
+    file_name = uploaded_file.name
     uploaded_file = None;  # Clear the uploaded file to free up memory
     # Compress audio
     compressed_audio_io = io.BytesIO()
@@ -58,11 +58,11 @@ if uploaded_file is not None and not st.session_state.processed:
     # with open("input_video.mp4", "wb") as f:
     #     f.write(audio_bytes)
     # subprocess.run(["ffmpeg", "-i", "input_video.mp4", "-vcodec", "libx264", "-crf", "28", compressed_video_path])
-
+    transcription = ""
     with st.spinner('Processing...'):
         # Process entire audio file
         result = pipe(compressed_audio_io.read())
-        transcription = result["text"]
+        transcription += result["text"]
 
     st.success('Done!')
     st.title("Result")
@@ -70,7 +70,7 @@ if uploaded_file is not None and not st.session_state.processed:
         st.info(transcription)
 
     # Create download button for transcription
-    filename_without_extension = os.path.splitext(uploaded_file.name)[0]
+    filename_without_extension = os.path.splitext(file_name)[0]
     output_file_name = f"{filename_without_extension}_transcription.txt"
     st.download_button(
         label="Download Transcription",
@@ -78,14 +78,5 @@ if uploaded_file is not None and not st.session_state.processed:
         mime="text/plain",
         data=transcription.encode('utf-8')
     )
-
-    # Create download button for compressed video
-    with open(compressed_video_path, "rb") as f:
-        st.download_button(
-            label="Download Compressed Video",
-            file_name=compressed_video_path,
-            mime="video/mp4",
-            data=f.read()
-        )
 
     st.session_state.processed = True

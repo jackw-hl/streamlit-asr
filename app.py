3d7c6c6d4455bd4435ae48647c423eee167d7910
Jack Wong <24000526g@connect.polyu.hk>
2024-11-06 01:35:38 +0000

    debug

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import torch
 import streamlit as st
 import io
 import os
-import subprocess
-from pydub import AudioSegment
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline, GenerationConfig
+from pydub import AudioSegment
 
-st.title("Cantonese ASR Transcription")
+st.title("Cantonese ASR")
 
 if 'processed' not in st.session_state:
     st.session_state.processed = False
@@ -14,13 +13,13 @@ if 'processed' not in st.session_state:
 uploaded_file = st.file_uploader("Upload your video file", type=["mp4"], key="file_uploader")
 
 if uploaded_file is not None:
-    st.session_state.processed = False
-
-if uploaded_file is not None and not st.session_state.processed:
-    lang = "zh"
+    lang = "cantonese"
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    model_id = "alvanlii/whisper-small-cantonese"
+    # "openai/whisper-large-v3-turbo"
+    # "openai/whisper-large-v3"
+    # model_id = "alvanlii/whisper-small-cantonese"
+    model_id = "openai/whisper-large-v3"
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
     )
@@ -39,44 +38,43 @@ if uploaded_file is not None and not st.session_state.processed:
         tokenizer=processor.tokenizer,
         feature_extractor=processor.feature_extractor,
         torch_dtype=torch_dtype,
-        device=device
+        device=device,
+        return_timestamps=True
     )
     pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(language=lang, task="transcribe")
+
+
+
 
     # Process audio
     audio_bytes = uploaded_file.read()
     audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="mp4")
-    file_name = uploaded_file.name
-    uploaded_file = None;  # Clear the uploaded file to free up memory
     # Compress audio
     compressed_audio_io = io.BytesIO()
     audio.export(compressed_audio_io, format='mp3', bitrate='64k')
     compressed_audio_io.seek(0)
 
-    # Compress video using ffmpeg
-    # compressed_video_path = "compressed_video.mp4"
-    # with open("input_video.mp4", "wb") as f:
-    #     f.write(audio_bytes)
-    # subprocess.run(["ffmpeg", "-i", "input_video.mp4", "-vcodec", "libx264", "-crf", "28", compressed_video_path])
-    transcription = ""
+    audio = None
+    audio_bytes = None
+
+    file_name = uploaded_file.name
+    uploaded_file = None;  # Clear the uploaded file to free up memory
+
     with st.spinner('Processing...'):
-        # Process entire audio file
-        result = pipe(compressed_audio_io.read())
-        transcription += result["text"]
+        transcription = pipe(compressed_audio_io.read())['text']
 
     st.success('Done!')
     st.title("Result")
     with st.expander("Transcription"):
         st.info(transcription)
 
-    # Create download button for transcription
+    # Create download button
     filename_without_extension = os.path.splitext(file_name)[0]
     output_file_name = f"{filename_without_extension}_transcription.txt"
+
     st.download_button(
         label="Download Transcription",
         file_name=output_file_name,
         mime="text/plain",
         data=transcription.encode('utf-8')
     )
-
-    st.session_state.processed = True
